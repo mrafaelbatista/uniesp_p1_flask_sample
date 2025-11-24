44390d7f4451dd8d1daf5ad6c13fdc506e2ec154
Messias R. Batista <mrafaelbatista@gmail.com>
2025-11-24 13:39:27 -0300

    Alterar termo adicionado.

--- app.py
+++ app.py
@@ -77,6 +77,41 @@ def excluir_termo(termo_id):
     return redirect(url_for('glossario'))
 
 
+@app.route('/alterar_termo/<int:termo_id>')
+def alterar_termo(termo_id):
+    with open('bd_glossario.csv', 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        linhas = list(reader)
+        termo_para_editar = None
+        for i, linha in enumerate(linhas):
+            if i == termo_id:
+                termo_para_editar = {'id': i, 'termo': linha[0], 'definicao': linha[1]}
+                break
+    
+    if termo_para_editar:
+        return render_template('alterar_termo.html', termo=termo_para_editar)
+    else:
+        return "Termo não encontrado", 404
+
+
+@app.route('/salvar_termo/<int:termo_id>', methods=['POST'])
+def salvar_termo(termo_id):
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open('bd_glossario.csv', 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        linhas = list(reader)
+
+    if 0 <= termo_id < len(linhas):
+        linhas[termo_id] = [termo, definicao]
+
+    with open('bd_glossario.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
+
 
 if __name__ == '__main__':
     app.run()
